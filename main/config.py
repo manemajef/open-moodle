27b4a7245086fe6ef2c51e4132b728b3555c4d03
manemajef <rotem.semah@gmail.com>
2025-10-28 21:06:38 +0200

    have option to login to tau instead of moodle

--- main/config.py
+++ main/config.py
@@ -1,15 +1,16 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 config = {
     "login_url": "https://moodle.tau.ac.il/auth/saml2/login.php",
+    "url": "https://moodle.tau.ac.il/local/mycourses/",
     "selectors": {
         "username": "Ecom_User_ID",
         "password": "Ecom_Password",
-        "id": "Ecom_User_Pid"
+        "id": "Ecom_User_Pid",
     },
     "username": os.getenv("USERNAME"),
     "id": os.getenv("ID"),
-    "password": os.getenv("PASSWORD")
+    "password": os.getenv("PASSWORD"),
 }
